a70055d5d9ad096c9a457da727aa216385ff33e2
Leo AD <talkto@theleoad.com>
2019-10-26 16:18:56 -0300

    feat: create theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  //colors
+  black: "#030303",
+  chatelle: "bbafca",
+  grape: "#331855",
+  white: "#fff",
+
+  //media queries
+  tablet: "980px",
+  mobile: "767px",
+}
