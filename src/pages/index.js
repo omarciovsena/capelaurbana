aa8469433599a3201e5b9064c6a0326a3ca7d7ff
Leo AD <talkto@theleoad.com>
2019-10-26 22:12:31 -0300

    chore: organize components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
-import Footer from "../components/Footer"
 import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+import ParallaxSection from "../components/ParallaxSection"
+
+import { theme } from "../styles/theme"
+
+import Brand from "../images/capela-urbana-logo.svg"
+import WorldMap from "../images/world-map.svg"
+import ContributeImage from "../images/bg-contribute.jpg"
+import SocialLeagueImage from "../images/bg-social-league.jpg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
     <Newsletter />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ParallaxSection
+      bgColor={theme.breakerBay}
+      bgImage={SocialLeagueImage}
+      title="Liga Social"
+      image={WorldMap}
+      imageRight
+      descriptions={[
+        "Nunc porta erat ut lectus posuere molestie. Vestibulum risusligula, rhoncus eleifend. Nunc porta erat ut lectus posueremolestie. Vestibulum risus ligula, rhoncus eleifend.",
+      ]}
+    />
+    <ParallaxSection
+      bgColor={theme.daintree2}
+      bgImage={ContributeImage}
+      title="Como Contribuir"
+      image={Brand}
+      descriptions={[
+        "Nunc porta erat ut lectus posuere molestie. Vestibulum risusligula, rhoncus eleifend. Nunc porta erat ut lectus posueremolestie. Vestibulum risus ligula, rhoncus eleifend.",
+        "Comunidade Capela Urbana CNPJ: 22.345.234/0001-89 <br /> Banco do Brasil - Agência 3410 <br /> Conta corrente 23423-8",
+      ]}
+    />
+    <Footer />
   </Layout>
 )
 
